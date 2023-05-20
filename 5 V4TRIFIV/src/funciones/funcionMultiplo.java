95ceaf162c2f0859a79c79b654cebfccefbc5a93
Jorge Melgarejo <melgarejojorge57@gmail.com>
2023-05-20 15:09:26 -0600

    Se añade funciones de V4TRIFIV

---
+++ 5 V4TRIFIV/src/funciones/funcionMultiplo.java
@@ -0,0 +1,15 @@
+package funciones;
+public class funcionMultiplo {
+    public String retornoMultiplo(int num){
+        if (num%3==0 & num%5==0){
+            return "TRIFIV";
+        }
+        else if (num%3==0){
+            return "TRI";
+        }
+        else if (num%5==0){
+            return "FIV";
+        }
+        return null;
+    }
+}
